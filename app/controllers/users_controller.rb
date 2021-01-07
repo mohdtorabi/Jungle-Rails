6f961bbc78ecf571ce7726ae7df682833d0bb927
Mohammad <mitq8.mt@gmail.com>
2021-01-07 21:15:53 +0000

    login/sign up completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @newUser = User.new(user_params)
+    if @newUser.save
+      session[:user_id] = @newUser.id
+      puts @newUser.inspect
       redirect_to '/'
     else
       redirect_to '/signup'
