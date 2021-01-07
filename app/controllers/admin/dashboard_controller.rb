c2a5e5d97abfac28b93dbf1b3e616eefc6760aff
Mohammad <mitq8.mt@gmail.com>
2021-01-07 05:22:36 +0000

    user auth added, need to add it to login and specific pages later

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "yoyo", password: "secret"
-
+  before_filter :authorize
   def show
     @products = Product.all
     @category = Category.all
